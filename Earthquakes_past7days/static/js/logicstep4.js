c49f051d940f805776f84b01e10a995c7ce8fa60
lerok3108 <lera.hamm@yahoo.com>
2022-04-02 11:58:23 -0400

    updates.

--- Earthquakes_past7days/static/js/logicstep4.js
+++ Earthquakes_past7days/static/js/logicstep4.js
@@ -20,9 +20,16 @@ let baseMaps = {
     "Satellite": satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
 
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
 
-
+  
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
     center: [39.5, -98.5],
@@ -30,8 +37,9 @@ let map = L.map('mapid', {
     layers: [streets]
 })
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 
 // Retrieve the earthquake GeoJSON data.
@@ -95,6 +103,8 @@ L.geoJSON(data, {
     onEachFeature: function(feature, layer) {
     layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
   }
-}).addTo(map);
+}).addTo(earthquakes);
 
+//add earthquakes data to map
+earthquakes.addTo(map);
 }); 
